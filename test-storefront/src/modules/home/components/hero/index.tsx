1e22c5f15e18aab23ad895b3d446e171cc85c94d
Petri <35950897+Pastamamba@users.noreply.github.com>
2024-11-16 21:04:26 +0200

    Hero page

--- test-storefront/src/modules/home/components/hero/index.tsx
+++ test-storefront/src/modules/home/components/hero/index.tsx
@@ -1,4 +1,3 @@
-import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
@@ -6,27 +5,19 @@ const Hero = () => {
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Ecommerce Starter Template
+          <Heading level="h1" className="text-5xl font-bold leading-tight">
+            Shop the best Deals on Top Brands & Unique Finds
           </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Powered by Medusa and Next.js
+          <Heading level="h2" className="text-gray-600 text-lg">
+            From everyday essentials to rare gems, we've got something for
+            everyone.
           </Heading>
         </span>
         <a
           href="https://github.com/medusajs/nextjs-starter-medusa"
           target="_blank"
         >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
+          <Button variant="primary">Shop now</Button>
         </a>
       </div>
     </div>
